8459bcdf2cbea5487b11d9176ed76d6cf51cf454
Tanzim Hossain <tanzimhossain2@gmail.com>
2023-10-04 21:05:13 +0600

    Add: cartIcon and css

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -12,6 +12,7 @@ const Layout = (props) => {
             rel="stylesheet"
             href="https://bootswatch.com/5/flatly/bootstrap.min.css"
           />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         </Head>
         <Header />
         {props.children}

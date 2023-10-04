2312fa62b3f6aed6575cdaa0e8a898e794193a6f
Tanzim Hossain <tanzimhossain2@gmail.com>
2023-10-04 17:41:51 +0600

    Add:  Single Product Page

--- pages/product.js
+++ pages/product.js
@@ -3,13 +3,48 @@ import { withRouter } from 'next/router';
 import client from "../components/ApolloClient";
 import gql from 'graphql-tag';
 
+/**
+ * This component renders a single product. It receives the product as props from the getInitialProps function.
+ * @param {*} props 
+ * @returns 
+ */
 const Product = (props) => {
-  console.log(props);
+  const { product } = props;
+
+  /**
+   * it uses the dangerouslySetInnerHTML prop to render the product description. so that the HTML tags are rendered as HTML and not as text.
+   * @param {*} htmlString 
+   * @returns 
+   * @see https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml
+   * 
+   */
+  const renderHTML = (htmlString) => {
+    return { __html: htmlString };
+  };
+
   return (
-    <div>product</div>
+    <Layout>
+      {
+        product ?
+          <div className="card bg-light mb-3 p-5" >
+            <div className="card-header">{product.name}</div>
+            <div className="card-body">
+              <h4 className="card-title">{product.name}</h4>
+              <img src={product.image.sourceUrl} alt="product Image" srcSet={product.image.srcSet} width="400px" />
+              <div dangerouslySetInnerHTML={renderHTML(product.description)} />
+            </div>
+          </div>
+          : null
+      }
+    </Layout>
   )
 }
 
+/**
+ * This function fetches a single product from WooCommerce and passes it to the Product component as props.
+ * @param {*} context 
+ * @returns 
+ */
 Product.getInitialProps = async function (context) {
   let { query: { slug } } = context;
   const id = slug ? slug.split('-').pop() : context.query.id; 
@@ -21,6 +56,31 @@ Product.getInitialProps = async function (context) {
         name
         slug
         description
+        image {
+          uri
+          title
+          srcSet
+          sourceUrl
+        }
+        ... on VariableProduct {
+          id
+          name
+          price
+          productId
+        }
+        ... on ExternalProduct {
+          id
+          name
+          price
+          productId
+        }
+        ... on GroupProduct {
+          id
+          name
+          price
+          productId
+        }
+      
       }
     }
   `;
--- utils/query.js
+++ utils/query.js
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag';
 
-const GET_PRODUCTS = gql`
+const GET_ALL_PRODUCTS = gql`
 query {
   products(first: 20) {
     nodes {
@@ -49,4 +49,41 @@ query {
   }
 }`;
 
-export default GET_PRODUCTS;
+
+export const GET_PRODUCT_BY_SLUG = gql`
+query GetProduct($productId: ID!) {
+  product(idType: DATABASE_ID, id: $productId) {
+    id
+    name
+    slug
+    description
+    image {
+      uri
+      title
+      srcSet
+      sourceUrl
+    }
+    ... on VariableProduct {
+      id
+      name
+      price
+      productId
+    }
+    ... on ExternalProduct {
+      id
+      name
+      price
+      productId
+    }
+    ... on GroupProduct {
+      id
+      name
+      price
+      productId
+    }
+  
+  }
+}
+`;
+
+export default GET_ALL_PRODUCTS;

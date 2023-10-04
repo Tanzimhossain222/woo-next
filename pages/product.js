a302502cfd4fd3e71b37449fa807bf2280ea2e3a
Tanzim Hossain <tanzimhossain2@gmail.com>
2023-10-04 16:02:35 +0600

    Fixed: Query for product.js  and fix issue  for single Product Page

--- pages/product.js
+++ pages/product.js
@@ -1,11 +1,36 @@
 import Layout from "../components/Layout";
 import { withRouter } from 'next/router';
 import client from "../components/ApolloClient";
+import gql from 'graphql-tag';
 
-const product = () => {
+const Product = (props) => {
+  console.log(props);
   return (
     <div>product</div>
   )
 }
 
-export default product
+Product.getInitialProps = async function (context) {
+  let { query: { slug } } = context;
+  const id = slug ? slug.split('-').pop() : context.query.id; 
+
+  const PRODUCT_QUERY = gql`
+    query GetProduct($productId: ID!) {
+      product(idType: DATABASE_ID, id: $productId) {
+        id
+        name
+        slug
+        description
+      }
+    }
+  `;
+
+  const res = await client.query({
+    query: PRODUCT_QUERY,
+    variables: { productId: id }
+  });
+
+  return { product: res.data.product }
+}
+
+export default withRouter(Product);
